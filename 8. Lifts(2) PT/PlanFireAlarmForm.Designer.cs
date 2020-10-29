bb158caead489919f017c52bebad77c949d21617
vladFedurko <fedurko.v@gmail.com>
2020-10-29 21:18:53 +0300

    ADD: create form

---
+++ 8. Lifts(2) PT/PlanFireAlarmForm.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace _8.Lifts_2__PT
+{
+    partial class PlanFireAlarmForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PlanFireAlarmForm));
+            this.SuspendLayout();
+            // 
+            // PlanFireAlarmForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 253);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PlanFireAlarmForm";
+            this.Text = "PlanFireAlarmForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
