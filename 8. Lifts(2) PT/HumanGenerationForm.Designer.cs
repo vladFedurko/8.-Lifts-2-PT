206cc0364b1ab4d97e760c9233e0ee1edd326468
ghbdtncjctl <rakovec.andrey@mail.ru>
2020-10-30 11:16:22 +0300

    ADD:: fixed border

--- 8. Lifts(2) PT/HumanGenerationForm.Designer.cs
+++ 8. Lifts(2) PT/HumanGenerationForm.Designer.cs
@@ -54,22 +54,24 @@
             this.humanGenerationTable.Controls.Add(this.num_of_humansLabel, 0, 0);
             this.humanGenerationTable.Controls.Add(this.finite_floorLabel, 2, 0);
             this.humanGenerationTable.Controls.Add(this.initial_floorLabel, 1, 0);
-            this.humanGenerationTable.Location = new System.Drawing.Point(12, 12);
+            this.humanGenerationTable.Location = new System.Drawing.Point(9, 10);
+            this.humanGenerationTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.humanGenerationTable.Name = "humanGenerationTable";
-            this.humanGenerationTable.Padding = new System.Windows.Forms.Padding(0, 0, 10, 0);
+            this.humanGenerationTable.Padding = new System.Windows.Forms.Padding(0, 0, 8, 0);
             this.humanGenerationTable.RowCount = 2;
             this.humanGenerationTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.humanGenerationTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
-            this.humanGenerationTable.Size = new System.Drawing.Size(1095, 179);
+            this.humanGenerationTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.humanGenerationTable.Size = new System.Drawing.Size(821, 145);
             this.humanGenerationTable.TabIndex = 0;
             // 
             // timeLabel
             // 
             this.timeLabel.AutoSize = true;
             this.timeLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.timeLabel.Location = new System.Drawing.Point(817, 1);
+            this.timeLabel.Location = new System.Drawing.Point(612, 1);
+            this.timeLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.timeLabel.Name = "timeLabel";
-            this.timeLabel.Size = new System.Drawing.Size(264, 20);
+            this.timeLabel.Size = new System.Drawing.Size(198, 20);
             this.timeLabel.TabIndex = 4;
             this.timeLabel.Text = "Через (секунд)";
             this.timeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -78,9 +80,10 @@
             // 
             this.num_of_humansLabel.AutoSize = true;
             this.num_of_humansLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.num_of_humansLabel.Location = new System.Drawing.Point(4, 1);
+            this.num_of_humansLabel.Location = new System.Drawing.Point(3, 1);
+            this.num_of_humansLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.num_of_humansLabel.Name = "num_of_humansLabel";
-            this.num_of_humansLabel.Size = new System.Drawing.Size(264, 20);
+            this.num_of_humansLabel.Size = new System.Drawing.Size(198, 20);
             this.num_of_humansLabel.TabIndex = 1;
             this.num_of_humansLabel.Text = "Число создаваемых людей";
             this.num_of_humansLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -89,9 +92,10 @@
             // 
             this.finite_floorLabel.AutoSize = true;
             this.finite_floorLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.finite_floorLabel.Location = new System.Drawing.Point(546, 1);
+            this.finite_floorLabel.Location = new System.Drawing.Point(409, 1);
+            this.finite_floorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.finite_floorLabel.Name = "finite_floorLabel";
-            this.finite_floorLabel.Size = new System.Drawing.Size(264, 20);
+            this.finite_floorLabel.Size = new System.Drawing.Size(198, 20);
             this.finite_floorLabel.TabIndex = 3;
             this.finite_floorLabel.Text = "Конечный этаж";
             this.finite_floorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -100,9 +104,10 @@
             // 
             this.initial_floorLabel.AutoSize = true;
             this.initial_floorLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.initial_floorLabel.Location = new System.Drawing.Point(275, 1);
+            this.initial_floorLabel.Location = new System.Drawing.Point(206, 1);
+            this.initial_floorLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.initial_floorLabel.Name = "initial_floorLabel";
-            this.initial_floorLabel.Size = new System.Drawing.Size(264, 20);
+            this.initial_floorLabel.Size = new System.Drawing.Size(198, 20);
             this.initial_floorLabel.TabIndex = 2;
             this.initial_floorLabel.Text = "Начальный этаж";
             this.initial_floorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -110,9 +115,10 @@
             // addButton
             // 
             this.addButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.addButton.Location = new System.Drawing.Point(335, 198);
+            this.addButton.Location = new System.Drawing.Point(251, 161);
+            this.addButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.addButton.Name = "addButton";
-            this.addButton.Size = new System.Drawing.Size(163, 28);
+            this.addButton.Size = new System.Drawing.Size(122, 23);
             this.addButton.TabIndex = 1;
             this.addButton.Text = "Добавить правило...";
             this.addButton.UseVisualStyleBackColor = true;
@@ -121,9 +127,10 @@
             // saveButton
             // 
             this.saveButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.saveButton.Location = new System.Drawing.Point(521, 198);
+            this.saveButton.Location = new System.Drawing.Point(391, 161);
+            this.saveButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.saveButton.Name = "saveButton";
-            this.saveButton.Size = new System.Drawing.Size(100, 28);
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
             this.saveButton.TabIndex = 2;
             this.saveButton.Text = "Сохранить";
             this.saveButton.UseVisualStyleBackColor = true;
@@ -132,9 +139,10 @@
             // denyButton
             // 
             this.denyButton.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
-            this.denyButton.Location = new System.Drawing.Point(642, 197);
+            this.denyButton.Location = new System.Drawing.Point(482, 160);
+            this.denyButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.denyButton.Name = "denyButton";
-            this.denyButton.Size = new System.Drawing.Size(100, 28);
+            this.denyButton.Size = new System.Drawing.Size(75, 23);
             this.denyButton.TabIndex = 3;
             this.denyButton.Text = "Отмена";
             this.denyButton.UseVisualStyleBackColor = true;
@@ -142,15 +150,14 @@
             // 
             // HumanGenerationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1119, 233);
+            this.ClientSize = new System.Drawing.Size(839, 189);
             this.Controls.Add(this.denyButton);
             this.Controls.Add(this.saveButton);
             this.Controls.Add(this.addButton);
             this.Controls.Add(this.humanGenerationTable);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "HumanGenerationForm";
             this.Text = "HumanGenerationForm";
             this.Deactivate += new System.EventHandler(this.HumanGenerationForm_Deactivate);
