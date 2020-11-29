ede6d698d2cf008bd5ca09a90a8cd72297d5c4b4
vladFedurko <fedurko.v@gmail.com>
2020-11-29 12:27:58 +0300

    ADD: interfaces iView and IMainView and presenter for MainView (without any methods)

---
+++ Presenters/IMainVIew.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _8.Lifts_2__PT;
+
+namespace Presenters
+{
+    interface IMainView : IView
+    {
+
+        void ShowState();
+
+        void SetParameters(int floors, int lifts);
+
+    }
+}
