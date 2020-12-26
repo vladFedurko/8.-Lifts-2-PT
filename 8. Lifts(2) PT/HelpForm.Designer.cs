a1774343de6b4a69474724f19593698fd2f539e8
vladFedurko <fedurko.v@gmail.com>
2020-12-26 14:26:47 +0300

    FIX: Grammar bugs in help form

--- 8. Lifts(2) PT/HelpForm.Designer.cs
+++ 8. Lifts(2) PT/HelpForm.Designer.cs
@@ -43,91 +43,101 @@
             // helpLabel
             // 
             this.helpLabel.AutoSize = true;
-            this.helpLabel.Location = new System.Drawing.Point(138, 9);
+            this.helpLabel.Location = new System.Drawing.Point(240, 9);
+            this.helpLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.helpLabel.Name = "helpLabel";
-            this.helpLabel.Size = new System.Drawing.Size(168, 13);
+            this.helpLabel.Size = new System.Drawing.Size(262, 17);
             this.helpLabel.TabIndex = 0;
-            this.helpLabel.Text = "This is all help we can deliver you!";
+            this.helpLabel.Text = "This is all the help we can deliver to you!";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 35);
+            this.label1.Location = new System.Drawing.Point(19, 43);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(277, 13);
+            this.label1.Size = new System.Drawing.Size(370, 17);
             this.label1.TabIndex = 1;
-            this.label1.Text = "This is simulation of system consisting of lifts and humans.";
+            this.label1.Text = "This is a system simulating behaviour of lifts and humans.";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Location = new System.Drawing.Point(17, 64);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(314, 13);
+            this.label2.Size = new System.Drawing.Size(419, 17);
             this.label2.TabIndex = 2;
             this.label2.Text = "Lifts are displayed in red while closed and in yellow while opened.";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 69);
+            this.label3.Location = new System.Drawing.Point(17, 85);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(231, 13);
+            this.label3.Size = new System.Drawing.Size(314, 17);
             this.label3.TabIndex = 3;
-            this.label3.Text = "Humans are displayed with numbers in the table";
+            this.label3.Text = "Humans are displayed with numbers in the table.";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 86);
+            this.label4.Location = new System.Drawing.Point(17, 106);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(433, 13);
+            this.label4.Size = new System.Drawing.Size(628, 17);
             this.label4.TabIndex = 4;
-            this.label4.Text = "You can create people using button \"Create human\" or define the rules of their ge" +
-    "neration.";
+            this.label4.Text = "You can create people by using button \"Create human\" or by defining the rules of " +
+    "their generation.";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 103);
+            this.label5.Location = new System.Drawing.Point(17, 127);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(434, 13);
+            this.label5.Size = new System.Drawing.Size(697, 17);
             this.label5.TabIndex = 5;
-            this.label5.Text = "You can stop simulation only when all the lifts are stopped, closed and there are" +
-    " no people.";
+            this.label5.Text = "You can stop the simulation only when all the lifts are stopped, closed, and ther" +
+    "e are no people in the system.";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(13, 120);
+            this.label6.Location = new System.Drawing.Point(17, 148);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(384, 13);
+            this.label6.Size = new System.Drawing.Size(585, 17);
             this.label6.TabIndex = 6;
-            this.label6.Text = "You can export generaton rules and system parameters to the file or import them.";
+            this.label6.Text = "You can export generaton rules and system parameters into a file or import them f" +
+    "rom a file.";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(13, 137);
+            this.label7.Location = new System.Drawing.Point(17, 169);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(302, 13);
+            this.label7.Size = new System.Drawing.Size(404, 17);
             this.label7.TabIndex = 7;
             this.label7.Text = "Statistics can be displayed only while the simulation is stopped.";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(13, 154);
+            this.label8.Location = new System.Drawing.Point(17, 190);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(184, 13);
+            this.label8.Size = new System.Drawing.Size(332, 17);
             this.label8.TabIndex = 8;
-            this.label8.Text = "Feedback us on example@email.com";
+            this.label8.Text = "Share your feedback with us — example@email.com";
             // 
             // HelpForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(455, 178);
+            this.ClientSize = new System.Drawing.Size(725, 219);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -138,6 +148,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.helpLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "HelpForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Help";
